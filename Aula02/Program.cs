f7da442889a501c1fe6177fbb6d59b306bf54efa
Altemar Farias <acf50mais@gmail.com>
2022-04-18 15:58:55 -0300

    Commit Inicial

---
+++ Aula02/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Aula02
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Jesus é Bom!");
+            if(args.GetLength(0)>0){
+                Console.WriteLine(args.GetValue(0));
+                Console.Write(args.GetValue(1));
+            }
+        }
+    }
+}
